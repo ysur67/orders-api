576a0757d5f6774368cf7f59f7f1d3791e6090d7
ysur67 <rus.muratow@gmail.com>
2022-08-04 10:48:39 +0300

    feat: run parse task every minute

--- apps/orders/tasks/parse_orders.py
+++ apps/orders/tasks/parse_orders.py
@@ -4,25 +4,51 @@ from typing import Any, Optional
 from apps.orders.service.parse.google_sheets_parser import GoogleSheetsParser
 from apps.orders.service.repositories.currency_repository.currencies import \
     Currency
+from apps.orders.service.repositories.currency_repository.exceptions import (
+    BadResponseFromCurrencyAPIException, MultipleCurrenciesInResponseException,
+    NoSuchCurrencyInResponseException)
 from apps.orders.service.repositories.currency_repository.repository import \
     BankOfRussiaCurrencyRepository
+from apps.orders.utils.logger import get_default_logger
 from celery import Task
 from django.conf import settings
-from django.core.management import BaseCommand
+from project import celery_app
 
 
 class ParseOrdersTask(Task):
 
+    name = "apps.orders.tasks.ParseOrdersTask"
+
     def run(self, *args, **kwargs):
-        repository = BankOfRussiaCurrencyRepository(
-            currency=Currency.DOLLAR,
-            url="https://www.cbr.ru/scripts/XML_daily.asp",
-            date=datetime.now(),
-        )
-        parser = GoogleSheetsParser(
-            creds_path=settings.BASE_DIR / 'credentials.json',
-            token_path=settings.BASE_DIR / 'token.json',
-            repository=repository,
-        )
-        parser.set_up()
-        parser.parse()
+        logger = get_default_logger("ParseOrdersTask")
+        logger.info("Launching the parser...")
+        try:
+            repository = BankOfRussiaCurrencyRepository(
+                currency=Currency.DOLLAR,
+                url="https://www.cbr.ru/scripts/XML_daily.asp",
+                date=datetime.now(),
+            )
+            parser = GoogleSheetsParser(
+                creds_path=settings.BASE_DIR / 'credentials.json',
+                token_path=settings.BASE_DIR / 'token.json',
+                repository=repository,
+            )
+            parser.set_up()
+            parser.parse()
+        except NotImplementedError as not_implemented_error:
+            logger.error(
+                "Not implemented error occurred. " +
+                "Are you sure you used the correct currencies? " +
+                "Error message: %s",
+                not_implemented_error
+            )
+        except (
+            BadResponseFromCurrencyAPIException,
+            MultipleCurrenciesInResponseException,
+            NoSuchCurrencyInResponseException,
+            AssertionError,
+        ) as error:
+            logger.error("An error occurred, %s. %s", type(error), error)
+
+
+celery_app.register_task(ParseOrdersTask())
--- project/celery.py
+++ project/celery.py
@@ -6,3 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.update(
+    beat_schedule={
+        'parse_orders_every_minute': {
+            'task': 'apps.orders.tasks.ParseOrdersTask',
+            'schedule': 60,
+        },
+    },
+)
